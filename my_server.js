2c10c71b438992d6f2530c1be2e234e14d3e3ceb
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-05-02 16:05:51 +0200

    OK : returning h1 works fine, need to add condition for get method now, ...

--- my_server.js
+++ my_server.js
@@ -6,7 +6,13 @@ const server = http.createServer(
 
         // console.log("We received a connection");
 
-        console.log("HELLO WORLD DIEUVEILLE");
+        // console.log("HELLO WORLD DIEUVEILLE");
+        
+        console.log(req);
+        console.log(res);
+
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write("<h1> HELLO WORLD DIEUVEILLE </h1>");
         
         res.end;
     }
