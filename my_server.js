f02b2318785aceb078bd1d3f7ce31639fb1840fe
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-05-02 16:14:21 +0200

    OK: tested condition method GET in if statement, ...

--- my_server.js
+++ my_server.js
@@ -4,15 +4,22 @@ const http = require("http");
 const server = http.createServer( 
     (req, res) =>{
 
-        // console.log("We received a connection");
+        if (req.method === "GET") 
+        {
 
-        // console.log("HELLO WORLD DIEUVEILLE");
-        
-        console.log(req);
-        console.log(res);
+            // console.log("We received a connection");
 
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.write("<h1> HELLO WORLD DIEUVEILLE </h1>");
+            // console.log("HELLO WORLD DIEUVEILLE");
+            
+            console.log(req);
+            console.log(req.url);
+            console.log(req.method);
+
+            console.log(res);
+
+            res.writeHead(200, {'content-type': 'text/html'});
+            res.write("<h1> HELLO WORLD DIEUVEILLE </h1>");
+        }
         
         res.end;
     }
