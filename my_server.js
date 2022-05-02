0dd92d3eb7d0d23a263c6d0a4934211f98e184cd
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-05-02 15:46:02 +0200

    OK : finished Question1 (section1), Hello World DIEUVEILLE listening at 5000, ...

--- my_server.js
+++ my_server.js
@@ -3,7 +3,9 @@ const http = require("http");
 
 const server = http.createServer( 
     (req, res) =>{
+
         // console.log("We received a connection");
+
         console.log("HELLO WORLD DIEUVEILLE");
         
         res.end;
