b13eacda6555cfef1c57135101187bc922b6f8c3
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-05-02 16:17:10 +0200

    KO: had to fix end function bug, my_server.js line 21 ...

--- my_server.js
+++ my_server.js
@@ -6,22 +6,19 @@ const server = http.createServer(
 
         if (req.method === "GET") 
         {
-
             // console.log("We received a connection");
-
-            // console.log("HELLO WORLD DIEUVEILLE");
             
-            console.log(req);
-            console.log(req.url);
-            console.log(req.method);
+            // console.log(req);
+            // console.log(req.url);
+            // console.log(req.method);
 
-            console.log(res);
+            // console.log(res);
 
             res.writeHead(200, {'content-type': 'text/html'});
             res.write("<h1> HELLO WORLD DIEUVEILLE </h1>");
         }
         
-        res.end;
+        res.end();
     }
 );
 
